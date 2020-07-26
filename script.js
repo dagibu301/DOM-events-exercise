1bd77659f08733b16df45bf437c0cae4d309b051
David Giraldo <davidgiraldobueno@gmail.com>
2020-07-26 11:45:48 -0500

    ES6

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
 
-function inputLength() {
+inputLength = ()=> {
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
+createListElement = ()=> {
+	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
 }
 
-function addListAfterClick() {
+addListAfterClick = ()=> {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
-function addListAfterKeypress(event) {
+addListAfterKeypress = (event)=> {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
