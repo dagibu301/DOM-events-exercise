52395a4e34429def41b4175b35b23e7096fb2a73
David Giraldo <davidgiraldobueno@gmail.com>
2020-07-26 17:15:07 -0500

    remove item onClick Delete button

--- script.js
+++ script.js
@@ -4,46 +4,46 @@ const ul = document.querySelector("ul");
 const liItems = document.getElementsByTagName("li");
 const items = ul.getElementsByTagName("li");
 
-
 for (let i = 0; i < liItems.length; i++) {
-	const button = document.createElement('button');
-	button.innerHTML = 'Delete';
-	liItems[i].appendChild(button);
-
+  const button = document.createElement("button");
+  button.innerHTML = "Delete";
+  liItems[i].appendChild(button);
 }
 
-inputLength = ()=> {
-	return input.value.length;
-}
+inputLength = () => {
+  return input.value.length;
+};
 
-createListElement = ()=> {
-	const li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
+createListElement = () => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+};
 
-addListAfterClick = ()=> {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
+addListAfterClick = () => {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+};
 
-addListAfterKeypress = (event)=> {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
+addListAfterKeypress = (event) => {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+};
 
-toggleDone = (e) => {
-	if (e.target.tagName === "LI") {
-        e.target.classList.toggle("done");
-    }
-}
-
+onClickList = (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("done");
+  } else if (e.target.tagName === "BUTTON") {
+	console.log(e.target);
+	e.target.parentNode.remove();
+  }
+};
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-ul.addEventListener("click", toggleDone);
+ul.addEventListener("click", onClickList);
